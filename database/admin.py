5a2713369ad7b3619a627045ea7b089df97a48df
santosh <santoshg.seo@gmail.com>
2021-03-14 13:16:34 +0530

    Models created

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Medicine)
+admin.site.register(MedicalDetails)
+admin.site.register(Employee)
+admin.site.register(Bill)
+admin.site.register(EmployeeSalary)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+admin.site.register(EmployeeBank)
